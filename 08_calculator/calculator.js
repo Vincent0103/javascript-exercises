0efc584cd3913ec21a826524fcd06a8b6009aaef
Vincent0103 <vincentkanagasundaram@gmail.com>
2023-07-06 14:22:28 +0200

    Finish 08_calculator exercise

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,34 @@
-const add = function() {
-	
+const add = (num, num1) => {
+	return num + num1;
 };
 
-const subtract = function() {
-	
+const subtract = (num, num1) => {
+	return num - num1;
 };
 
-const sum = function() {
-	
+const sum = (arrayNums) => {
+  let result = 0;
+  for (let num of arrayNums) result += num;
+  return result;
 };
 
-const multiply = function() {
-
+const multiply = (firstNum, ...argsNum) => {
+  let result = firstNum;
+  for (let num of argsNum) result *= num;
+  return result;
 };
 
-const power = function() {
-	
+const power = (num, num1) => {
+  return Math.pow(num, num1);
 };
 
-const factorial = function() {
-	
+const factorial = (num) => {
+  if (num === 0) return 1;
+
+  let result = 1;
+  for (let i = 1; i <= num; i++) result *= i;
+
+  return result;
 };
 
 // Do not edit below this line
