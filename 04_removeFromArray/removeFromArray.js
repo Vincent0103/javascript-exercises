45f4b0ee2e33c5bd864b1703db09cdc99f96ff39
Vincent0103 <vincentkanagasundaram@gmail.com>
2023-07-06 10:57:19 +0200

    Finish 04_removeFromArray exercise

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,11 @@
-const removeFromArray = function() {
+const removeFromArray = (array, ...nums) => {
+    for (let num of nums) {
+        for (let i = 0; i < array.length; i++) {
+            if (num === array[i]) array.splice(i, 1);
+        }
+    }
 
+    return array;
 };
 
 // Do not edit below this line
