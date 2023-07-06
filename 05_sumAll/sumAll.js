f2b6adb438ff7c0f5881b55945cc9b9478e6884b
Vincent0103 <vincentkanagasundaram@gmail.com>
2023-07-06 11:13:07 +0200

    Finish 05_sumAll exercise

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,24 @@
-const sumAll = function() {
+const sumAll = (num, num1) => {
+    let totalSum = 0;
+    let firstInt = 0;
+    let lastInt = 0;
 
+    if (!(Number.isInteger(num) && Number.isInteger(num1))) return "ERROR";
+    if (!(num >= 0 && num1 >= 0)) return "ERROR";
+
+    if (num > num1) {
+        firstInt = num1;
+        lastInt = num;
+    } else {
+        firstInt = num;
+        lastInt = num1;
+    }
+
+    for (let i = firstInt; i <= lastInt; i++) {
+        totalSum += i;
+    }
+
+    return totalSum;
 };
 
 // Do not edit below this line
