845bc747b7e49137688a4cc442c5b4215fe931a8
Vincent0103 <vincentkanagasundaram@gmail.com>
2023-07-06 14:06:13 +0200

    Finish 07_tempConversion exercise

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,15 @@
-const convertToCelsius = function() {
+const convertToCelsius = fahrenheitValue => {
+
+  // convert fahrenheit to celsius rounded to 1 decimal place
+  let celsiusValue = (fahrenheitValue - 32) * (5/9);
+  return Math.round(celsiusValue * 10) / 10;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = celsiusValue => {
+
+  // convert celsius to fahrenheit rounded to 1 decimal place
+  let fahrenheitValue = celsiusValue * (9/5) + 32;
+  return Math.round(fahrenheitValue * 10) / 10;
 };
 
 // Do not edit below this line
