107ec147da83669bde9574b9f84d3a1e01e4c13e
Vincent0103 <vincentkanagasundaram@gmail.com>
2023-07-11 21:16:56 +0200

    Finish 10_fibonacci exercise

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,24 @@
-const fibonacci = function() {
+const fibonacci = num => {
+    prevNums = [0, 0]
+    if (typeof num === "string") num = parseInt(num);
 
+    let fibonacciResult = 0;
+    if (num < 0) return "OOPS";
+    for (let i = 0; i < num; i++) {
+        fibonacciResult = 0;
+        prevNums.forEach((prevNum, index) => {
+            if (prevNum === 0) prevNums[index] = 1;
+            fibonacciResult += prevNum;
+            if (num === 1) fibonacciResult = 1;
+        })
+        prevNums[0] = prevNums[1];
+        prevNums[1] = fibonacciResult;
+    }
+
+    return fibonacciResult;
 };
+
+console.log(fibonacci("1"));
 
 // Do not edit below this line
 module.exports = fibonacci;
