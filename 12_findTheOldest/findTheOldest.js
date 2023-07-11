c4d0544c18366f3787a843d20d2f3e5f8eb3f544
Vincent0103 <vincentkanagasundaram@gmail.com>
2023-07-11 22:19:08 +0200

    Finish 12_findTheOldest exercise

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,15 @@
-const findTheOldest = function() {
-
+const findTheOldest = arr => {
+    let currentOldest = 0;
+    let oldest = "";
+    arr.forEach(item => {
+      if (item.yearOfDeath === undefined) item.yearOfDeath = (new Date).getFullYear();
+      let livingYears = item.yearOfDeath - item.yearOfBirth;
+      if (currentOldest < livingYears) {
+        currentOldest = livingYears;
+        oldest = item;
+      }
+    });
+    return oldest;
 };
 
 // Do not edit below this line
